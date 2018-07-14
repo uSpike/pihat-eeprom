d9b4fb3b9d68221d79cd460f7e4380512722a23e
Jordan Speicher <jordan@jspeicher.com>
2018-07-13 21:06:21 -0500

    checkin

--- pihat_eeprom.py
+++ pihat_eeprom.py
@@ -1,6 +1,5 @@
-import inspect
+import ctypes
 import io
-import pathlib
 import struct
 from uuid import uuid4
 
@@ -23,7 +22,9 @@ def _unpack(stream, length, fmt):
     data = stream.read(length)
     try:
         if not len(data) == length:
-            raise ValueError('Data too short, saw {} expected {}'.format(len(data), length))
+            raise ValueError(
+                "Data too short, saw {} expected {}".format(len(data), length)
+            )
         unpacked = struct.unpack(fmt, data)
         if len(unpacked) == 1:
             return unpacked[0]
@@ -47,7 +48,7 @@ def _set_bits(value, mask, bits):
     return (value & (~mask)) | ((bits << shift) & mask)
 
 
-class Struct(object):
+class Struct(ctyles.LittleEndianStructure):
     """
     Represents some data object that can parse data and be built back into a binary stream.
     """
@@ -70,8 +71,8 @@ class Struct(object):
     def __repr__(self):
         """Create a __repr__ string for an object"""
         values = ((name, getattr(self, name)) for name in self._repr_attrs)
-        kw = ', '.join('{}={}'.format(k, v) for k, v in values)
-        return '{cls}({kw})'.format(cls=self.__class__.__name__, kw=kw)
+        kw = ", ".join("{}={}".format(k, v) for k, v in values)
+        return "{cls}({kw})".format(cls=self.__class__.__name__, kw=kw)
 
     __str__ = __repr__
 
@@ -88,9 +89,9 @@ class Header(Struct):
     4       eeplen      total length in bytes of all eeprom data (including this header)
     """
 
-    _repr_attrs = ('signature', 'version', 'numatoms', 'eeplen')
+    _repr_attrs = ("signature", "version", "numatoms", "eeplen")
 
-    def __init__(self, signature=b'R-Pi', version=0x1, numatoms=0, eeplen=0):
+    def __init__(self, signature=b"R-Pi", version=0x1, numatoms=0, eeplen=0):
         self.signature = signature
         self.version = version
         self.numatoms = numatoms
@@ -98,11 +99,13 @@ class Header(Struct):
 
     @classmethod
     def parse(cls, stream):
-        signature, version, _, numatoms, eeplen = _unpack(stream, 12, '<4sBBHI')
+        signature, version, _, numatoms, eeplen = _unpack(stream, 12, "<4sBBHI")
         return cls(signature, version, numatoms, eeplen)
 
     def build(self):
-        return struct.pack('<4sBBHI', self.signature, self.version, 0, self.numatoms, self.eeplen)
+        return struct.pack(
+            "<4sBBHI", self.signature, self.version, 0, self.numatoms, self.eeplen
+        )
 
 
 class VendorInfoAtomData(Struct):
@@ -119,15 +122,21 @@ class VendorInfoAtomData(Struct):
     Y       pstr        ASCII product string e.g. "Special Sensor Board"
     """
 
-    _repr_attrs = ('uuid', 'product_id', 'product_version', 'vendor_string', 'product_string')
+    _repr_attrs = (
+        "uuid",
+        "product_id",
+        "product_version",
+        "vendor_string",
+        "product_string",
+    )
 
     def __init__(
         self,
         uuid=None,
         product_id=0,
         product_version=0,
-        vendor_string=b'',
-        product_string=b'',
+        vendor_string=b"",
+        product_string=b"",
         vendor_slen=0,
         product_slen=0,
     ):
@@ -144,9 +153,9 @@ class VendorInfoAtomData(Struct):
 
     @classmethod
     def parse(cls, stream, length=None):
-        uuid, pid, pver, vslen, pslen = _unpack(stream, 22, '<16sHHBB')
-        vstr = _unpack(stream, vslen, '{}s'.format(vslen))
-        pstr = _unpack(stream, pslen, '{}s'.format(pslen))
+        uuid, pid, pver, vslen, pslen = _unpack(stream, 22, "<16sHHBB")
+        vstr = _unpack(stream, vslen, "{}s".format(vslen))
+        pstr = _unpack(stream, pslen, "{}s".format(pslen))
         return cls(
             uuid=uuid,
             product_id=pid,
@@ -161,7 +170,9 @@ class VendorInfoAtomData(Struct):
         self.vendor_slen = len(self.vendor_string)
         self.product_slen = len(self.product_string)
         return struct.pack(
-            '<16sHHBB{vslen}s{pslen}s'.format(vslen=self.vendor_slen, pslen=self.product_slen),
+            "<16sHHBB{vslen}s{pslen}s".format(
+                vslen=self.vendor_slen, pslen=self.product_slen
+            ),
             self.uuid,
             self.product_id,
             self.product_version,
@@ -172,27 +183,27 @@ class VendorInfoAtomData(Struct):
         )
 
 
-class ArgMapMixin(object):
-    _arg_map = {}
+def _make_bitfield_property(attrname, bits, value_map):
+    value_map_rev = {v: k for k, v in value_map.items()}  # reversed
 
-    def _get_arg(self, attrname, bits, name):
+    def _get(self):
         attr = getattr(self, attrname)
         val = _get_bits(attr, bits)
-        amap = {v: k for k, v in self._arg_map[name].items()}  # reverse
-        if not val in amap:
-            raise KeyError('{} got invalid value: {}'.format(name, val))
-        return amap[val]
+        if not val in value_map_rev:
+            raise KeyError("invalid value: {}".format(val))
+        return value_map_rev[val]
 
-    def _set_arg(self, attrname, bits, name, value):
+    def _set(self, value):
         attr = getattr(self, attrname)
-        amap = self._arg_map[name]
-        if value not in amap:
-            raise KeyError('{} set invalid value: {}'.format(name, value))
-        new_value = _set_bits(field, bits, amap[value])
+        if value not in value_map:
+            raise KeyError("invalid value: {}".format(value))
+        new_value = _set_bits(attr, bits, value_map[value])
         setattr(self, attrname, new_value)
 
+    return property(_get, _set)
 
-class GPIOPin(Struct, ArgMapMixin):
+
+class GPIOPin(Struct):
     """
     GPIO Pin
 
@@ -223,51 +234,42 @@ class GPIOPin(Struct, ArgMapMixin):
 
     """
 
-    _arg_map = {
-        'func_sel': {'input': 0, 'output': 1, 'alt0': 4, 'alt1': 5, 'alt2': 6, 'alt3': 7, 'alt4': 3, 'alt5': 2},
-        'pulltype': {'default': 0, 'pullup': 1, 'pulldown': 2, 'nopull': 3},
-        'is_used': {False: 0, True: 1},
-    }
-
-    _repr_attrs = ('func_sel', 'pulltype', 'is_used')
+    _repr_attrs = ("func_sel", "pulltype", "is_used")
 
     def __init__(self, data):
         self.data = data
 
     @classmethod
     def parse(cls, stream):
-        data = _unpack(stream, 1, '<B')
+        data = _unpack(stream, 1, "<B")
         return cls(data)
 
     def build(self):
-        return self.data.to_bytes(1, 'little')
+        return self.data.to_bytes(1, "little")
 
-    @property
-    def func_sel(self):
-        return self._get_arg('data', 0x03, 'func_sel')
+    func_sel = _make_bitfield_property(
+        "data",
+        0x03,
+        {
+            "input": 0,
+            "output": 1,
+            "alt0": 4,
+            "alt1": 5,
+            "alt2": 6,
+            "alt3": 7,
+            "alt4": 3,
+            "alt5": 2,
+        },
+    )
 
-    @func_sel.setter
-    def func_sel(self, val):
-        self._set_arg('data', 0x03, 'func_sel', val)
+    pulltype = _make_bitfield_property(
+        "data", 0x60, {"default": 0, "pullup": 1, "pulldown": 2, "nopull": 3}
+    )
 
-    @property
-    def pulltype(self):
-        return self._get_arg('data', 0x60, 'pulltype')
-
-    @pulltype.setter
-    def pulltype(self, val):
-        self._set_arg('data', 0x60, 'pulltype', val)
-
-    @property
-    def is_used(self):
-        return self._get_arg('data', 0x80, 'is_used')
-
-    @is_used.setter
-    def is_used(self, val):
-        self._set_arg('data', 0x80, 'is_used', val)
+    is_used = _make_bitfield_property("data", 0x80, {False: 0, True: 1})
 
 
-class GPIOMapAtomData(Struct, ArgMapMixin):
+class GPIOMapAtomData(Struct):
     """
     GPIO Map Atom Data
 
@@ -281,31 +283,24 @@ class GPIOMapAtomData(Struct, ArgMapMixin):
     `slew` can have the following values:
 
       - `default`
-      - `enable`
-      - `disable`
+      - `enabled`
+      - `disabled`
 
    `hysteresis` can have the following values:
 
       - `default`
-      - `enable`
-      - `disable`
+      - `enabled`
+      - `disabled`
 
    `back_power` can have the following values:
 
-      - `disable`
-      - `enable_1.3A`
-      - `enable_2A`
+      - `disabled`
+      - `enabled_1.3A`
+      - `enabled_2A`
 
     """
 
-    _arg_map = {
-        'drive': {'default': 0, **{n: n for n in range(1, 9)}},
-        'slew': {'default': 0, 'enable': 1, 'disable': 2},
-        'hysteresis': {'default': 0, 'enable': 1, 'disable': 2},
-        'back_power': {'disable': 0, 'enable_1.3A': 1, 'enable_2A': 2},
-    }
-
-    _repr_attrs = ('drive', 'slew', 'hysteresis', 'back_power', 'pins')
+    _repr_attrs = ("drive", "slew", "hysteresis", "back_power", "pins")
 
     def __init__(self, bank_drive=0, power=0, pins=None):
         self.bank_drive = bank_drive
@@ -315,46 +310,30 @@ class GPIOMapAtomData(Struct, ArgMapMixin):
         else:
             self.pins = pins
 
-    @property
-    def drive(self):
-        return self._get_arg('bank_drive', 0x0f, 'drive')
+    drive = _make_bitfield_property(
+        "bank_drive", 0x0f, {"default": 0, **{n: n for n in range(1, 9)}}
+    )
 
-    @drive.setter
-    def drive(self, val):
-        self._set_arg('bank_drive', 0x0f, 'drive', val)
+    slew = _make_bitfield_property(
+        "bank_drive", 0x30, {"default": 0, "enabled": 1, "disabled": 2}
+    )
 
-    @property
-    def slew(self):
-        return self._get_arg('bank_drive', 0x30, 'slew')
+    hysteresis = _make_bitfield_property(
+        "bank_drive", 0xc0, {"default": 0, "enabled": 1, "disabled": 2}
+    )
 
-    @slew.setter
-    def slew(self, val):
-        self._set_arg('bank_drive', 0x30, 'slew', val)
-
-    @property
-    def hysteresis(self):
-        return self._get_arg('bank_drive', 0xc0, 'hysteresis')
-
-    @hysteresis.setter
-    def hysteresis(self, val):
-        self._set_arg('bank_drive', 0xc0, 'hysteresis', val)
-
-    @property
-    def back_power(self):
-        return self._get_arg('power', 0x03, 'back_power')
-
-    @back_power.setter
-    def back_power(self, val):
-        self._set_arg('power', 0x03, 'back_power', val)
+    back_power = _make_bitfield_property(
+        "power", 0x03, {"disabled": 0, "enabled_1.3A": 1, "enabled_2A": 2}
+    )
 
     @classmethod
     def parse(cls, stream, length=None):
-        bank_drive, power = _unpack(stream, 2, '<BB')
+        bank_drive, power = _unpack(stream, 2, "<BB")
         return cls(bank_drive, power, [GPIOPin.parse(stream) for _ in range(28)])
 
     def build(self):
-        pins = b''.join(p.build() for p in self.pins)
-        return struct.pack('<BB28s', self.bank_drive, self.power, pins)
+        pins = b"".join(p.build() for p in self.pins)
+        return struct.pack("<BB28s", self.bank_drive, self.power, pins)
 
 
 class DeviceTreeAtomData(Struct):
@@ -364,18 +343,18 @@ class DeviceTreeAtomData(Struct):
     Format is just a blob.
     """
 
-    _repr_attrs = ('blob',)
+    _repr_attrs = ("blob",)
 
     def __init__(self, blob):
         self.blob = blob
 
     @classmethod
     def parse(cls, stream, length):
-        blob = _unpack(stream, length, '<{}s'.format(length))
+        blob = _unpack(stream, length, "<{}s".format(length))
         return cls(blob)
 
     def build(self):
-        return struct.pack('<{}s'.format(len(self.blob)), self.blob)
+        return struct.pack("<{}s".format(len(self.blob)), self.blob)
 
 
 class CustomDataAtomData(Struct):
@@ -385,18 +364,18 @@ class CustomDataAtomData(Struct):
     Format is just a blob.
     """
 
-    _repr_attrs = ('blob',)
+    _repr_attrs = ("blob",)
 
     def __init__(self, blob):
         self.blob = blob
 
     @classmethod
     def parse(cls, stream, length):
-        blob = _unpack(stream, length, '<{}s'.format(length))
+        blob = _unpack(stream, length, "<{}s".format(length))
         return cls(blob)
 
     def build(self):
-        return struct.pack('<{}s'.format(len(self.blob)), self.blob)
+        return struct.pack("<{}s".format(len(self.blob)), self.blob)
 
 
 class Atom(Struct):
@@ -411,16 +390,16 @@ class Atom(Struct):
     2       crc16       CRC-16-CCITT of entire atom (type, count, dlen, data)
     """
 
-    _repr_attrs = ('type_name', 'data', 'crc')
+    _repr_attrs = ("type_name", "data", "crc")
 
     atom_types = {
-        0x1: ('vendor_info', VendorInfoAtomData),
-        0x2: ('gpio_map', GPIOMapAtomData),
-        0x3: ('devicetree', DeviceTreeAtomData),
-        0x4: ('custom_data', CustomDataAtomData),
+        0x1: ("vendor_info", VendorInfoAtomData),
+        0x2: ("gpio_map", GPIOMapAtomData),
+        0x3: ("devicetree", DeviceTreeAtomData),
+        0x4: ("custom_data", CustomDataAtomData),
     }
 
-    def __init__(self, type, count, dlen, data, crc):
+    def __init__(self, type, count, dlen, data, crc=0):
         self.type = type
         self.count = count
         self.dlen = dlen
@@ -436,7 +415,7 @@ class Atom(Struct):
 
     @property
     def _data_raw(self):
-        if hasattr(self.data, 'build'):
+        if hasattr(self.data, "build"):
             return self.data.build()
         else:
             return self.data
@@ -444,7 +423,7 @@ class Atom(Struct):
     @classmethod
     def parse(cls, stream, length=None):
         # First, unpack up to data length
-        type_, count, dlen = _unpack(stream, 8, '<HHI')
+        type_, count, dlen = _unpack(stream, 8, "<HHI")
 
         # If possible, get a specific class for the atom data and let that
         # class parse the data.
@@ -454,19 +433,25 @@ class Atom(Struct):
             data = type_class.parse(stream, length=dlen - 2)
         else:
             # don't know the class, so data is plain bytes
-            data = _unpack(stream, dlen - 2, '{dlen}s'.format(dlen=dlen - 2))
+            data = _unpack(stream, dlen - 2, "{dlen}s".format(dlen=dlen - 2))
 
         # Finish up
-        crc = _unpack(stream, 2, 'H')
+        crc = _unpack(stream, 2, "H")
         return cls(type_, count, dlen, data, crc)
 
     def build(self):
         data_raw = self._data_raw
         self.dlen = len(data_raw) + 2  # includes CRC
-        data = struct.pack('<HHI{dlen}s'.format(dlen=self.dlen - 2), self.type, self.count, self.dlen, data_raw)
-        crc_func = crcmod.predefined.mkCrcFun('crc-16')
+        data = struct.pack(
+            "<HHI{dlen}s".format(dlen=self.dlen - 2),
+            self.type,
+            self.count,
+            self.dlen,
+            data_raw,
+        )
+        crc_func = crcmod.predefined.mkCrcFun("crc-16")
         self.crc = crc_func(data)
-        return struct.pack('<{}sH'.format(len(data)), data, self.crc)
+        return struct.pack("<{}sH".format(len(data)), data, self.crc)
 
 
 class EEPROM(Struct):
@@ -478,22 +463,30 @@ class EEPROM(Struct):
     N       Atoms
     """
 
-    _repr_attrs = ('header', 'atoms')
+    _repr_attrs = ("header", "atoms")
 
-    def __init__(self, data):
-        # make a copy of the data in a stream format
-        self._stream = io.BytesIO(data)
-        self.header = Header.parse(self._stream)
-        self.atoms = []
-        for numatom in range(self.header.numatoms):
-            self.atoms.append(Atom.parse(self._stream))
+    def __init__(self, header=None, atoms=None):
+        if header is not None:
+            self.header = header
+        else:
+            self.header = Header()
+        if atoms is not None:
+            self.atoms = atoms
+        else:
+            self.atoms = []
 
     @classmethod
     def parse(cls, data):
         """
         Load binary data to create an EEPROM
         """
-        return cls(data)
+        stream = io.BytesIO(data)
+        header = Header.parse(stream)
+        atoms = []
+        for numatom in range(header.numatoms):
+            atoms.append(Atom.parse(stream))
+        header.numatoms = numatom
+        return cls(header, atoms)
 
     def update(self):
         for count, atom in enumerate(self.atoms):
@@ -524,17 +517,16 @@ class EEPROM(Struct):
 
         This creates 'vendor_info' and 'gpio_map' atoms by default.
         """
-        header = Header()
-        inst = cls(header.build())
-        inst.add_atom('vendor_info', **kw.pop('vendor_info', {}))
-        inst.add_atom('gpio_map', **kw.pop('gpio_map', {}))
+        inst = cls()
+        inst.add_atom("vendor_info")
+        inst.add_atom("gpio_map")
         inst.update()
         return inst
 
     @property
     def atoms_type(self):
         """
-        Returns a dictionary of atoms by type string name.
+        Dictionary of atoms by type string name.
         """
         return {a.type_name: a for a in self.atoms}
 
@@ -549,21 +541,21 @@ class EEPROM(Struct):
             raise KeyError(type_name)
         data = type_cls(*args, **kwargs)
         data_build = data.build()
-        atom = Atom(type_val, len(self.atoms), len(data_build), data, 0)
+        atom = Atom(type_val, len(self.atoms), len(data_build), data)
         self.atoms.append(atom)
         self.update()
         return atom
 
 
-if __name__ == '__main__':
-    with open('eeprom.bin', 'rb') as f:
+if __name__ == "__main__":
+    with open("eeprom.bin", "rb") as f:
         eep = EEPROM.parse(f.read())
 
-    eep.atoms_type['vendor_info'].data.vendor_string = b'foo'
-    if not 'custom_data' in eep.atoms_type:
-        eep.add_atom('custom_data', b'hello world')
+    eep.atoms_type["vendor_info"].data.vendor_string = b"foo"
+    if not "custom_data" in eep.atoms_type:
+        eep.add_atom("custom_data", b"hello world")
     eep.update()
-    with open('eeprom_new.bin', 'wb') as f:
+    with open("eeprom_new.bin", "wb") as f:
         f.write(eep.build())
 
     # eep = EEPROM.create()
